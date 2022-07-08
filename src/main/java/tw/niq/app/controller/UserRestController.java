8daa06311d0fe50bd71e221bb665008e41fcca4c
nick.chen <sakanasan20@gmail.com>
2022-07-08 09:41:54 +0800

    Add query string reading in get users method

--- src/main/java/tw/niq/app/controller/UserRestController.java
+++ src/main/java/tw/niq/app/controller/UserRestController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
 public class UserRestController {
+	
+	@GetMapping
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, 
+			@RequestParam(value = "limit", defaultValue = "10") int limit, 
+			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+		return "get users was called with page = " + page + " and limit = " + limit + " and srot = " + sort;
+	}
 
 	@GetMapping(path = "/{userId}")
 	public String getUser(@PathVariable String userId) {
