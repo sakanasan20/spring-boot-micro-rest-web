d9c4cadebd9abe6dd19b7991d15ba1f6091b9d70
Nick <sakanasan20@gmail.com>
2022-07-08 13:58:11 +0800

    Create user service exception (#16)

--- src/main/java/tw/niq/app/exception/handler/UserExceptionHandler.java
+++ src/main/java/tw/niq/app/exception/handler/UserExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import tw.niq.app.exception.UserServiceException;
 import tw.niq.app.model.response.ErrorMessage;
 
 @ControllerAdvice
@@ -29,6 +30,18 @@ public class UserExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(value = {NullPointerException.class})
 	public ResponseEntity<Object> handleNullPointerException(Exception ex, WebRequest request) {
+		
+		String message = ex.getLocalizedMessage();
+		
+		if (message == null) message = ex.toString();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), message);
+		
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value = {UserServiceException.class})
+	public ResponseEntity<Object> handleUserServiceException(Exception ex, WebRequest request) {
 		
 		String message = ex.getLocalizedMessage();
 		
