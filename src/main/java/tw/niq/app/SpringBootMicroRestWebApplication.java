4b6d5bfbb50e825cdc06ccd78f2be002258a3015
nick.chen <sakanasan20@gmail.com>
2022-07-08 08:53:27 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootMicroRestWebApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroRestWebApplication.class, args);
+	}
+
+}
